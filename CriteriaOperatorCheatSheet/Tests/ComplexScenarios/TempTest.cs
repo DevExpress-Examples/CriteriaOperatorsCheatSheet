a713d979d279613f9886073caea45fb80dcadf67
andrey <andrey.kozhevnikov@devexpress.com>
2022-08-16 15:04:14 +0400

    remove uneeded tests

---
+++ CriteriaOperatorCheatSheet/Tests/ComplexScenarios/TempTest.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpo;
+using dxTestSolutionXPO.Module.BusinessObjects;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dxTestSolutionXPO.Tests {
+    [TestFixture]
+    public class TempTests : BaseTest {
+
+    
+    }
+}
