42408a7b4a012f0896c3395dc2ab91275eb25e1b
andrey <andrey.kozhevnikov@devexpress.com>
2022-09-14 17:44:24 +0400

    IsNewObject

---
+++ CriteriaOperatorCheatSheetXAF/CriteriaOperatorCheatSheetXAF.Blazor.Server/Controllers/CustomControllerBlazor.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using System;
+using System.Linq;
+
+namespace dxTestSolution.Module.Controllers {
+    public class CustomBlazorController : ViewController {
+        public CustomBlazorController() {
+            var myAction1 = new SimpleAction(this, "MyBlazorAction1", null);
+            myAction1.Execute += MyAction1_Execute;
+            myAction1.TargetObjectsCriteria = "IsNewObject(@This)";
+        }
+
+        private void MyAction1_Execute(object sender, SimpleActionExecuteEventArgs e) {
+           
+        }
+    }
+}
