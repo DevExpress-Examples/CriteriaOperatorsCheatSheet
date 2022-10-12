c9a9e47610162767f3736a5ed3d1d35e4b9a4a91
andrey <andrey.kozhevnikov@devexpress.com>
2022-10-12 17:00:30 +0400

    exists.left

--- CriteriaOperatorCheatSheet/Tests/Aggregate_Exists.cs
+++ CriteriaOperatorCheatSheet/Tests/Aggregate_Exists.cs
@@ -17,9 +17,10 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.Parse("OrderItems.Exists()");
-            CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName0");
-            var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
+            var criterion = 
+                CriteriaOperator.Parse("OrderItems.Exists()");
+            var resultCriterion = new BinaryOperator(nameof(Order.OrderName), "FirstName0");
+            var result3 = uow.Evaluate<Order>(criterion, resultCriterion);
             //assert
             Assert.AreEqual(true, result3);
         }
@@ -29,9 +30,10 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.Parse("OrderItems.Exists()");
-            CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName1");
-            var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
+            var criterion = 
+                CriteriaOperator.Parse("OrderItems.Exists()");
+            var resultCriterion = new BinaryOperator(nameof(Order.OrderName), "FirstName1");
+            var result3 = uow.Evaluate<Order>(criterion, resultCriterion);
             //assert
             Assert.AreEqual(false, result3);
         }
@@ -41,7 +43,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = new AggregateOperand(nameof(Order.OrderItems), Aggregate.Exists);
+            CriteriaOperator criterion = 
+                new AggregateOperand(nameof(Order.OrderItems), Aggregate.Exists);
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName0");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
@@ -53,7 +56,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.FromLambda<Order>(o => o.OrderItems.Any());
+            CriteriaOperator criterion = 
+                CriteriaOperator.FromLambda<Order>(o => o.OrderItems.Any());
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName0");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
@@ -65,7 +69,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.Parse("[OrderItems][ItemPrice=20]");
+            CriteriaOperator criterion = 
+                CriteriaOperator.Parse("[OrderItems][ItemPrice=20]");
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName2");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
@@ -77,7 +82,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.Parse("[OrderItems][ItemPrice=30]");
+            CriteriaOperator criterion = 
+                CriteriaOperator.Parse("[OrderItems][ItemPrice=30]");
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName2");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
@@ -89,7 +95,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = new AggregateOperand(nameof(Order.OrderItems), Aggregate.Exists,new BinaryOperator(nameof(OrderItem.ItemPrice),20));
+            CriteriaOperator criterion = 
+                new AggregateOperand(nameof(Order.OrderItems), Aggregate.Exists,new BinaryOperator(nameof(OrderItem.ItemPrice),20));
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName2");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
@@ -101,7 +108,8 @@ namespace dxTestSolutionXPO.Tests {
             PopulateSelectFromCollectionForCount();
             var uow = new UnitOfWork();
             //act
-            CriteriaOperator criterion = CriteriaOperator.FromLambda<Order>(o => o.OrderItems.Any(oi=>oi.ItemPrice==20)); ;
+            CriteriaOperator criterion = 
+                CriteriaOperator.FromLambda<Order>(o => o.OrderItems.Any(oi=>oi.ItemPrice==20)); ;
             CriteriaOperator filterParentCollection = new BinaryOperator(nameof(Order.OrderName), "FirstName2");
             var result3 = uow.Evaluate<Order>(criterion, filterParentCollection);
             //assert
